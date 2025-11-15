15d8d6ada406860b6b388eb8f88e2fc2e3ceb05e
Muchaimercy <muchaimercy044@gmail.com>
2025-11-15 22:33:15 +0300

    Add match case calculator.py

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,5 +1,3 @@
-# Simple calculator using match-case
-
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Choose the operation (+, -, *, /): ")
